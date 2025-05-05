06599b5207061ec482cb1cbde483bc0e461e151d
Hassan Javed <hjaved.engineer@gmail.com>
2025-05-05 09:08:19 +0300

    Testing File

--- demo.py
+++ demo.py
@@ -2,8 +2,9 @@ def my_function():
     a = 10
     b = 20
     d = a + b
-    API_KEY = "a1b2c33d4e5f6g7h8i9jakblc"
-    return d, API_KEY
+    
+    return d
 
 
 my_function()
+API_KEY = "a1b2c33d4e5f6g7h8i9jakblc"
