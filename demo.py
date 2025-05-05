2aef165b48387adcd3dcb9bb6671503bfc40a529
Hassan Javed <hjaved.engineer@gmail.com>
2025-05-05 09:05:05 +0300

    Testing File

--- demo.py
+++ demo.py
@@ -2,8 +2,8 @@ def my_function():
     a = 10
     b = 20
     d = a + b
-    apikey = 'ABCDE-incorrect-key'
-    return d, apikey
+    API_KEY = 'ABCDE-incorrect-key'
+    return d, API_KEY
 
 
 my_function()
